d1f048363d579154d6067de0ba787f65968c3ff6
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-03-13 12:38:32 +0100

    Removing full path for setup
    Removed so that git actions can run correctly

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ echo "Starting setup"
 echo "Password might be needed..."
 
 echo "Making setup script executable..."
-chmod u+x ~/.config/general.sh
-chmod u+x ~/.config/brew.sh
-chmod u+x ~/.config/link_config.sh
+chmod u+x .config/general.sh
+chmod u+x .config/brew.sh
+chmod u+x .config/link_config.sh
 echo "chmod...done"
 
 echo "Installering defaults..."
-. ~/.config/general.sh
-. ~/.config/brew.sh
+. .config/general.sh
+. .config/brew.sh
 echo "Installing defaults...done"
 
 echo "Installing configs..."
-. ~/.config/link_config.sh
+. .config/link_config.sh
 echo "Configs...done"
