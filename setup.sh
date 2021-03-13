3226b978ed887299af4ba05529230c54c70d8251
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-03-13 14:56:57 +0100

    Move setup scripts to own folder

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ echo "Starting setup"
 echo "Password might be needed..."
 
 echo "Making setup script executable..."
-chmod u+x .config/general.sh
-chmod u+x .config/brew.sh
-chmod u+x .config/link_config.sh
+chmod u+x .setup_scripts/general.sh
+chmod u+x .setup_scripts/brew.sh
+chmod u+x .setup_scripts/link_config.sh
 echo "chmod...done"
 
 echo "Installering defaults..."
-./.config/general.sh
-./.config/brew.sh
+./.setup_scripts/general.sh
+./.setup_scripts/brew.sh
 echo "Installing defaults...done"
 
 echo "Installing configs..."
-./.config/link_config.sh
+./.setup_scripts/link_config.sh
 echo "Configs...done"
