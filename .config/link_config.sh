8ff777c72700b2261caeb12569797c8eca2cbd4b
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-03-13 13:14:48 +0100

    Fix linting in link_config

--- .config/link_config.sh
+++ .config/link_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Starting link_config.sh..."
-files=["$HOME/.pk10.zsh", "$HOME/.zshrc"]
+files=("$HOME/.pk10.zsh" "$HOME/.zshrc")
 
 echo "Removing default configs"
-for f in $files ; do
+for f in "${files[@]}" ; do
     if [ -f "$f" ] ; then
         rm "$f"
         echo "Removed...$f"
@@ -13,8 +13,8 @@ done
 echo "Removing defaults...done"
 
 echo "Linking configs with -s"
-ln -s $HOME/.config/pk10/.pk10.zsh $HOME/.pk10.zsh
-ln -s $HOME/.config/oh-my-zsh/.zshrc $HOME/.zshrc
+ln -s "$HOME"/.config/pk10/.pk10.zsh "$HOME"/.pk10.zsh
+ln -s "$HOME"/.config/oh-my-zsh/.zshrc "$HOME"/.zshrc
 echo "Linking...done"
 
 echo "Linking configs...done"
