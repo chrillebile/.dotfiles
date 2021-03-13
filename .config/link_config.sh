b7233d511fefe2f2ff64c29401edccc14fb440d8
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-03-13 13:06:40 +0100

    Fix linting problems

--- .config/link_config.sh
+++ .config/link_config.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Starting link_config.sh..."
-files = ["~/.pk10.zsh", "~/.zshrc"]
+files=["$HOME/.pk10.zsh", "$HOME/.zshrc"]
 
 echo "Removing default configs"
 for f in $files ; do
-    if [ -f $f ] ; then
-        rm $f
+    if [ -f "$f" ] ; then
+        rm "$f"
         echo "Removed...$f"
     fi 
 done
 echo "Removing defaults...done"
 
 echo "Linking configs with -s"
-ln -s ~/.config/pk10/.pk10.zsh ~/.pk10.zsh
-ln -s ~/.config/oh-my-zsh/.zshrc ~/.zshrc
+ln -s $HOME/.config/pk10/.pk10.zsh $HOME/.pk10.zsh
+ln -s $HOME/.config/oh-my-zsh/.zshrc $HOME/.zshrc
 echo "Linking...done"
 
 echo "Linking configs...done"
--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ chmod u+x .config/link_config.sh
 echo "chmod...done"
 
 echo "Installering defaults..."
-. .config/general.sh
-. .config/brew.sh
+./.config/general.sh
+./.config/brew.sh
 echo "Installing defaults...done"
 
 echo "Installing configs..."
-. .config/link_config.sh
+./.config/link_config.sh
 echo "Configs...done"
