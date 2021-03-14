e3c13762fdfbbf5807b1f2b03e6feb3c51d0fd3c
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-03-14 11:59:40 +0100

    Add documentation on how to start dnscrypt

--- .setup_scripts/link_config.sh
+++ .setup_scripts/link_config.sh
@@ -19,6 +19,8 @@ ln -s "$HOME"/.config/oh-my-zsh/.zshrc "$HOME"/.zshrc
 echo "Linking configs with their tag"
 if ! command -v dnscrypt-proxy &> /dev/null ;  then
     sudo dnscrypt-proxy -config ~/.config/dnscrypt-proxy/dnscrypt-proxy.toml
+    echo "To start dnscrypt-proxy run:"
+    echo "\tsudo brew services start dnscrypt-proxy"
 fi
 echo "Linking...done"
 
