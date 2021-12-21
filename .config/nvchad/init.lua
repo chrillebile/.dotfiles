7d0bbea70a23f483a33aadf2c9504c3c78215263
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-12-21 11:39:15 +0100

    Fix lspconfig
    Default config now works

--- .config/nvchad/init.lua
+++ .config/nvchad/init.lua
@@ -30,16 +30,6 @@ hooks.add("install_plugins", function(use)
 
    use {
       "williamboman/nvim-lsp-installer",
-      config = function()
-        local lsp_installer = require 'nvim-lsp-installer'
-
-        lsp_installer.on_server_ready(function(server)
-          local opts = {}
-
-          server:setup(opts)
-          vim.cmd [[ do User LspAttachBuffers ]]
-        end)
-      end,
       --further packer options
    }
 end)
--- .config/nvchad/plugins/lspconfig.lua
+++ .config/nvchad/plugins/lspconfig.lua
@@ -1,33 +1,45 @@
+
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
+   local lsp_installer = require "nvim-lsp-installer"
 
-   -- lspservers with default config
-
-   local servers = { "html", "cssls", "pyright" }
-
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
+   lsp_installer.on_server_ready(function(server)
+      local opts = {
          on_attach = attach,
          capabilities = capabilities,
          flags = {
             debounce_text_changes = 150,
          },
+         settings = {},
       }
-   end
-   
-   -- typescript
 
- lspconfig.tsserver.setup {
-      on_attach = function(client, bufnr)
-         client.resolved_capabilities.document_formatting = false
-         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-      end,
-   }
+--       if server.name == "rust_analyzer" then
+--          opts.settings = {
+--             ["rust-analyzer"] = {
+--                experimental = {
+--                   procAttrMacros = true,
+--                },
+--             },
+--          }
+-- 
+--          opts.on_attach = function(client, bufnr)
+--             local function buf_set_keymap(...)
+--                vim.api.nvim_buf_set_keymap(bufnr, ...)
+--             end
+-- 
+--             -- Run nvchad's attach
+--             attach(client, bufnr)
+-- 
+--             -- Use nvim-code-action-menu for code actions for rust
+--             buf_set_keymap(bufnr, "n", "<leader>ca", "lua vim.lsp.buf.range_code_action()<CR>", { noremap = true, silent = true })
+--             buf_set_keymap(bufnr, "v", "<leader>ca", "lua vim.lsp.buf.range_code_action()<CR>", { noremap = true, silent = true })
+--          end
+--       end
 
--- the above tsserver config will remvoe the tsserver's inbuilt formatting 
--- since I use null-ls with denofmt for formatting ts/js stuff.
+      server:setup(opts)
+      vim.cmd [[ do User LspAttachBuffers ]]
+   end)
 end
 
 return M
