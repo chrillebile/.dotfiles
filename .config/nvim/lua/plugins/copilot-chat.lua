831ecb341e55ad0d5a58f18f58422eff13eeb233
Christian Bilevits <chrillebile@users.noreply.github.com>
2025-01-04 12:00:41 +0100

    Add copilot config

---
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,23 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "canary",
+  cmd = "CopilotChat",
+  opts = function()
+    local user = vim.env.USER or "User"
+    user = user:sub(1, 1):upper() .. user:sub(2)
+    return {
+      model = "gpt-4o",
+      auto_insert_mode = true,
+      show_help = true,
+      question_header = "  " .. user .. " ",
+      answer_header = "  Copilot ",
+      window = {
+        width = 0.4,
+      },
+      selection = function(source)
+        local select = require("CopilotChat.select")
+        return select.visual(source) or select.buffer(source)
+      end,
+    }
+  end,
+}
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+      filetypes = {
+        ["*"] = true,
+      },
+    },
+  },
+}
